41fa5ade536305e01bb99917f268d81343940ccd
slowpoke <slowpoke@singularity.(none)>
2011-04-21 13:58:41 +0100

    Added a setup.py blackbox can now a selfinstalling module!

---
+++ setup.py
@@ -0,0 +1,41 @@
+#       _   _______ _____       ___  ________ _____ 
+#      | | / /  _  /  ___|      |  \/  |  _  /  ___|
+#      | |/ /| | | \ `--. ______| .  . | | | \ `--. 
+#      |    \| | | |`--. \______| |\/| | | | |`--. \
+#      | |\  \ \_/ /\__/ /      | |  | \ \_/ /\__/ /
+#      \_| \_/\___/\____/       \_|  |_/\___/\____/ 
+#      ______              _            _   _                 
+#      | ___ \            | |          | | (_)                
+#      | |_/ / __ ___   __| |_   _  ___| |_ _  ___  _ __  ___ 
+#      |  __/ '__/ _ \ / _` | | | |/ __| __| |/ _ \| '_ \/ __|
+#      | |  | | | (_) | (_| | |_| | (__| |_| | (_) | | | \__ \
+#      \_|  |_|  \___/ \__,_|\__,_|\___|\__|_|\___/|_| |_|___/
+
+# ASCII banner created with http://www.network-science.de/ascii/
+
+
+# blackbox IRC macros - setup.py
+
+# (c) 2011 Proxy of KOS-MOS Productions
+# Offical IRC channel: irc.datnode.net/#KOS-MOS
+# Offical repository on github: https://github.com/proxypoke/blackbox_IRC-macros
+
+# This program, hereafter called 'blackbox', 
+# is Free Software under the terms of the 
+# GNU General Public license, which can be found at 
+# http://www.gnu.org/copyleft/gpl.html.
+
+# Version 1.0 (beta)
+# blackbox is still in active developement.
+
+from distutils.core import setup
+
+setup(
+	name = 'blackbox',
+	version = '0.4',
+	author = 'Proxy',
+	description = 'A Python module to abstract and encapsulate the IRC protocol',
+	url = 'https://github.com/proxypoke/blackbox_IRC-macros',
+	license = 'GNU General Public License v3',
+	py_modules = ['blackbox', 'blackbox_oper'],
+	)
